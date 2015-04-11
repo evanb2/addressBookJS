5fb748338b66f4801306226960af9145cb5dceab
Evan Butler <evanb2@mac.com>
2015-04-10 19:38:07 -0700

    add jquery to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-var (name) = function(input) {
+$(document).ready(function() {
+    $("form#new-contact").submit(function(event) {
+        event.preventDefault();
 
-};
+        var inputtedFirstName = $("input#new-first-name").val();
+        var inputtedLastName = $("input#new-last-name").val();
+        var inputtedAddress = $("input#new-address").val();
+        var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress };
+
+        $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+
+        $("input#new-first-name").val("");
+        $("input#new-last-name").val("");
+        $("input#new-address").val("");
+        
+    });
+});
